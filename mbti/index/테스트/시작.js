fb45f7dfdbf9d74ef3730dc56601ba7219a10efb
csh <csh98419@naver.com>
2024-04-25 19:35:11 +0900

    no message

---
+++ mbti/index/테스트/시작.js
@@ -0,0 +1,7 @@
+document.getElementById('male').addEventListener('click', function() {
+    alert('남자를 클릭했습니다')
+  });
+  
+  document.getElementById('female').addEventListener('click', function() {
+    alert('여자를 클릭했습니다')
+  });
